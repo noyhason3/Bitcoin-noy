0ba25628d8ccda8f77ba0448f987ff3851667602
noyhason3 <noyhason3@gmail.com>
2021-05-01 14:37:07 +0300

    fixed build errors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost:3030'],
         credentials: true
     }
     app.use(cors(corsOptions))
@@ -50,6 +50,7 @@ connectSockets(http, session)
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/car/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
+app.use(express.static('public'))
 app.get('/**', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
